75a3b07d97273355736792b408eb51aafb2b0619
Dhananjay Kaushik <dhananjay@centilytics.com>
2021-05-03 19:40:28 +0530

    skeleton commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles.css'
+import IMAGE from './assets/logo512.png'
+
+export const App = () => {
+    return (
+        <>
+            <h1>React Typescript SASS Webpack Skeleton</h1>
+            <img src={IMAGE} alt="React Logo" />
+        </>
+    )
+}
